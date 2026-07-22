d320c18cee7310f4d4d1fb0e4672c35abff7167b
Vasumitra <pascal.cathleen1989@gmail.com>
2026-07-22 16:47:33 +0200

    première tentative de boucle (non fonctionnel)

--- ex1bis_dictionnaire.py
+++ ex1bis_dictionnaire.py
@@ -1,3 +1,4 @@
+
 # comment entrer chaque nouvelle entrée utilisateur dans le dictionnaire ? Boucle "while" ?
 # création d'un dictionnaire vide ?
 user_dict = {}
@@ -7,3 +8,9 @@ user_name = input("Quel est votre nom d'utilisateur ? ")
 user_score = input("Quel est votre score ? ")
 # stocker les deux input dans le dictionnaire
 user_dict[user_name] = user_score
+print(user_dict)
+
+while user_dict < 5:
+    user_name = input("Quel est votre nom d'utilisateur ? ")
+    user_score = input("Quel est votre score ? ")
+    print(user_dict)
