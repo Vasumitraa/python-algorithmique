9c0506d7a0ac620430cff4629f98baa2cbd7fdef
Vasumitra <pascal.cathleen1989@gmail.com>
2026-08-04 19:55:58 +0200

    break fonctionne

--- ex1bis_dictionnaire.py
+++ ex1bis_dictionnaire.py
@@ -11,9 +11,9 @@ print(user_dict)
 
 # faire une boucle pour que chaque entrée aille dans le dictionnaire :
 while user_name != "null":
+    user_name = input("Quel est votre nom d'utilisateur ? ")
     if user_name == "null":
         break
-    user_name = input("Quel est votre nom d'utilisateur ? ")
     user_score = input("Quel est votre score ? ")
     user_dict[user_name] = user_score
     print(user_dict)
