fdec0904ea51bf0f50dda1fe117cd4a8b8366768
Vasumitra <pascal.cathleen1989@gmail.com>
2026-08-05 14:23:51 +0200

    tentative d'utilisation de la méthode pop() (non fonctionnel)

--- ex1_dictionaire.py
+++ ex1_dictionaire.py
@@ -1,28 +1,29 @@
-# créer un dictionnaire "scores"
+# créer un dictionnaire "scores" :
 scores = {"MokoSempai": 16, "Grungi": 30, "Elocin03": 56}
-# créer une variable "pseudo" avec input utilisateur
+# créer une variable "pseudo" avec input utilisateur :
 pseudo = input("Quel est votre pseudo ? ")
-# créer une variable "score" avec input utilisateur
+# créer une variable "score" avec input utilisateur :
 score = int(input("Quel est votre score ? "))
 # faire rentrer les deux input dans le dictionnaire : dict["clé"] = valeur
 scores[pseudo] = score
-# afficher le score de "Elocin03" ainsi que le score du nouveau pseudo entré
+# afficher le score de "Elocin03" ainsi que le score du nouveau pseudo entré :
 print(scores["Elocin03"], scores[pseudo])
 
 # pour aller plus loin, j'aimerais trouver un moyen de: 
-# trier par clé
+# trier par clé :
 for pseudo in sorted(scores):
     print(pseudo)
-# trier par valeur
+# trier par valeur :
 for value in sorted(scores.values()):
     print(str(value))
-# afficher tout (clé + valeur)
+# afficher tout (clé + valeur) :
 for key, value in scores.items():
     print (key + " : " + str(value))
-# trier l'ensemble par clé
+# trier l'ensemble par clé :
 for key, value in sorted(scores.items()):
     print (key + " : " + str(value))
-# trier l'ensemble par valeur
+# trier l'ensemble par valeur :
+ 
  
 # comment entrer chaque nouvelle entrée utilisateur dans le dictionnaire ? Boucle "while" ?
 # création d'un dictionnaire vide :
--- ex2corr_dictionnaire.py
+++ ex2corr_dictionnaire.py
@@ -1,16 +1,18 @@
 # exercice moyenne:
-# création du dictionnaire
+# création du dictionnaire :
 club_rank = {"Marie": 1, "Bernard": 4, "François": 2, "Thomas": 12, "Hila": 15}
-# création d'une variable initialisée à 0 pour contenir les valeurs à additionner
+# suppression des valeurs supérieures à 10 :
+suppressed_values = club_rank.pop("value" > 10)
+# création d'une variable initialisée à 0 pour contenir les valeurs à additionner :
 sum_rank = 0
-# création d'une boucle for pour additionner les valeurs
+# création d'une boucle for pour additionner les valeurs :
 for value in club_rank.values():
     sum_rank += value
-# afficher la valeur de la variable
+# afficher la valeur de la variable :
 print(sum_rank)
-# diviser par le nombre de valeur pour avoir la moyenne
+# diviser par le nombre de valeur pour avoir la moyenne :
 average_rank = sum_rank / len(club_rank)
-# afficher la moyenne
+# afficher la moyenne :
 print("Les membres du club sportif ont un rang moyen de : " + str(average_rank))
 
 # est-ce qu'il existerait une méthode encore plus adéquate ? 
